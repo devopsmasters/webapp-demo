2f764b50db6b32bfb22d6e539728ab474981e293
Rajesh Gopal <rajeshsmyl@gmail.com>
2018-02-07 14:51:38 +0530

    update for zalenium

---
+++ src/test/java/de/codecentric/springbootsample/SeleniumIntegration.java
@@ -0,0 +1,37 @@
+package de.codecentric.springbootsample;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SeleniumIntegration {
+	
+	    @Test
+	    public void testExecution() throws IOException {
+	        
+	    	DesiredCapabilities caps = DesiredCapabilities.chrome();
+	        caps.setCapability("platform", "linux");
+	        caps.setCapability("version", "64.0.3282.119");
+	        
+	        WebDriver driver = new RemoteWebDriver(caps);
+	    	
+	        driver.get("http://localhost:8181/springdemo");
+	        
+	       
+	      String Actualtitle = driver.getTitle();
+	      System.out.println("Before Assetion " + Actualtitle);
+	      //it will compare actual title and expected title
+	      //assertEquals(Expectedtitle, Actualtitle);
+	      driver.quit();
+	    }
+	
+}
+
