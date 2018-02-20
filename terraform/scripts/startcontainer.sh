6c86bf78e50df444186729a71793ed4e1b7abae8
devopsmasters <33680369+devopsmasters@users.noreply.github.com>
2018-02-21 02:40:01 +0530

    Create startcontainer.sh

---
+++ terraform/scripts/startcontainer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+  
+#workaround to check if docker is running
+#while ! docker ps; do sleep 2; done
+echo "docker container running"
+#sudo docker run -d -p 8080:8080 rajeshgopal/rg-tomcat2:latest
